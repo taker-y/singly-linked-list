94abb56924e8638fead81b56f8b094884dd45063
taker-y <tak.10.3.blue@gmail.com>
2019-04-11 18:59:57 +0900

    [update] list.c #3
    add list_import function

--- list.c
+++ list.c
@@ -2,3 +2,35 @@
 #include <stdlib.h>
 
 #include "list.h"
+#include "file_reader.h"
+
+int list_import(node_t **list, char *path)
+{
+    FILE *fp;
+    node_t *root;
+    node_t *head;
+    char read_string[20];
+    
+    fp = open_file(path);
+
+    if (fgets(read_string, 20, fp) == NULL) {
+        printf("can't read file\n");
+        return -1;
+    } else {
+        root = malloc(sizeof(node_t));
+        root->data = strtol(read_string, NULL, 10);
+        head = root;
+        *list = root;
+
+        while (fgets(read_string, 20, fp) != NULL) {
+            head->next = malloc(sizeof(node_t));
+            head = head->next;
+
+            head->data = strtol(read_string, NULL, 10);
+        }
+    }
+
+    fclose(fp);
+
+    return 0;
+}
